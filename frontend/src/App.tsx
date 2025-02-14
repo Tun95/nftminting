8a5c450d9c4b722e2cfe36a595bc5445b3e33522
Tunji Akande <akandetunji3@gmail.com>
2025-02-14 12:12:41 +0100

    FEAT: added navbar and some icons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,14 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import NotFoundScreen from "./utilities/404 error/PageNotFound";
 import HomeScreen from "./screens/homescreen/HomeScreen";
+import NavBar from "./common/navbar/NavBar";
 
 function App() {
   return (
     <>
       <div className="app">
         <ToastContainer position="bottom-center" limit={1} />
-
+        <NavBar />
         <Routes>
           <Route path="*" element={<NotFoundScreen />} />
           <Route path="/" element={<HomeScreen />}></Route>
--- frontend/src/common/navbar/NavBar.tsx
+++ frontend/src/common/navbar/NavBar.tsx
@@ -1,18 +1,23 @@
+import { MingcuteWallet3Fill } from "../../assets/icons/Index";
 import "./styles.scss";
+import logo from "../../assets/nav/logo.png";
 
 function NavBar() {
   return (
     <div className="nav_bar">
       <div className="container">
-        <div className="content">
+        <div className="content c_flex">
           <div className="left">
             <div className="img">
-              <img src="" alt="" />
+              <img src={logo} alt="logo img" />
             </div>
           </div>
           <div className="right">
-            <div className="btn">
-              <div className="main_btn a_flex"></div>
+            <div className="btn l_flex">
+              <button className="main_btn a_flex">
+                <MingcuteWallet3Fill className="icon" />
+                <p>Connect Wallet</p>
+              </button>
             </div>
           </div>
         </div>
