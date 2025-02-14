afb4ececc296d9fef77eab81c2465224413c0656
Tunji Akande <akandetunji3@gmail.com>
2025-02-14 11:54:58 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+import cors from "cors";
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import { createServer } from "http";
+import helmet from "helmet";
+
+dotenv.config();
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    connectTimeoutMS: 1000000,
+  })
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const app = express();
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json({ limit: "10mb" }));
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE",
+    credentials: true,
+  })
+);
+
+app.use(
+  helmet({
+    crossOriginOpenerPolicy: { policy: "same-origin" },
+    crossOriginEmbedderPolicy: { policy: "require-corp" },
+  })
+);
+
+app.get("/", (req, res) => {
+  res.status(200).send("Server is alive");
+});
+
+//Routes
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const server = createServer(app);
+const port = process.env.PORT || 5000;
+
+// Increase timeout settings
+server.setTimeout(1000000);
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ frontend/src/common/navbar/NavBar.tsx
@@ -0,0 +1,24 @@
+import "./styles.scss";
+
+function NavBar() {
+  return (
+    <div className="nav_bar">
+      <div className="container">
+        <div className="content">
+          <div className="left">
+            <div className="img">
+              <img src="" alt="" />
+            </div>
+          </div>
+          <div className="right">
+            <div className="btn">
+              <div className="main_btn a_flex"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
